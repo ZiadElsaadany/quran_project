888d713d6591bc0c5a3174d49a7968d1897df88c
Ziad Elsdaadany <ziadelsaadany73@gmail.com>
2023-01-31 04:07:17 +0200

    add home view

---
+++ lib/view/home_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  static  const  String id ='HOME View ';
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [
+
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
