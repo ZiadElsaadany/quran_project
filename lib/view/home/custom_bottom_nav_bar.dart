1d0f05e6d37582c64d2c27103d5ccd6b818005ff
Ziad Elsdaadany <ziadelsaadany73@gmail.com>
2023-02-02 17:17:13 +0200

    custom bottom nav bar done

--- lib/view/home/custom_bottom_nav_bar.dart
+++ lib/view/home/custom_bottom_nav_bar.dart
@@ -1,20 +1,38 @@
-// import 'package:flutter/material.dart';
-//
-// class CustomBottomNavBar extends StatelessWidget {
-//   const CustomBottomNavBar({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return BottomNavigationBar(
-//       elevation: 0,
-//       items: [
-//         BottomNavigationBarItem(icon: Icon(Icons.settings,color: Colors.grey),label: ''),
-//
-//         BottomNavigationBarItem(icon: Icon(Icons.person,color: Colors.grey),label: ''),
-//         BottomNavigationBarItem(icon: Icon(Icons.search,color: Colors.grey),label: ''),
-//
-//         BottomNavigationBarItem(icon: Icon(Icons.home,color: Colors.grey,),label: '' ,),
-//       ],
-//     );
-//   }
-// }
+
+import 'package:flutter/material.dart';
+import 'package:quran_project/constant/color_constant.dart';
+
+import 'nav_bar_item.dart';
+
+class CustomBottomNavBar extends StatelessWidget {
+  const CustomBottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+          top: MediaQuery.of(context).size.height*0.01),
+      decoration: const BoxDecoration(
+        color:AppColorsConstant.primaryColor,
+        borderRadius: BorderRadius.only(
+          bottomRight: Radius.circular(10),
+          bottomLeft: Radius.circular(10),
+        )
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+         const BottomNavItem(icon: Icons.home, title: 'Home'),
+         const BottomNavItem(icon: Icons.home, title: 'Home'),
+      SizedBox(width: MediaQuery.of(context).size.width*0.1,),
+          const BottomNavItem(icon: Icons.home, title: 'Home'),
+          const BottomNavItem(icon: Icons.home, title: 'Home'),
+
+        ],
+      ),
+    );
+  }
+}
+
+
--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:quran_project/constant/color_constant.dart';
+import 'package:quran_project/view/home/custom_bottom_nav_bar.dart';
 
 
 import '../../controller/providers/quran_provider.dart';
@@ -24,25 +26,54 @@ class _HomeViewState extends State<HomeView> {
   }
   @override
   Widget build(BuildContext context) {
-    return   const Directionality(
+    return     Directionality(
       textDirection: TextDirection.rtl,
       child: SafeArea(
         child: Scaffold(
-          body: HomeViewBody(),
+          body: Column(
+            children: const [
+              Expanded(child: HomeViewBody()),
+              CustomBottomNavBar()
+            ],
+          ),
+          floatingActionButton: const CustomFloatingActionButton(),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
 
-          // floatingActionButton: FloatingActionButton(
-          //     onPressed: (){},
-          //
-          //     ),
-          // floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterDocked,
-          // bottomNavigationBar: BottomAppBar( //to margin between floating and bottom
-          //   clipBehavior: Clip.antiAlias, //to margin between floating and bottom
-          //   shape: const CircularNotchedRectangle(), //to margin between floating and bottom
-          //   notchMargin: 5,    //to margin between floating and bottom
-          //   child:CustomBottomNavBar(),
-          // ),
+
         ),
       ),
     );
+  }
+}
+
+  class CustomFloatingActionButton extends StatelessWidget {
+  const CustomFloatingActionButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+        shape: RoundedRectangleBorder(
+            side: const BorderSide(
+                color: Colors.white ,
+                width:2
+            ),
+            borderRadius: BorderRadius.circular(100)
+        ),
+        backgroundColor: AppColorsConstant.primaryColor,
+        onPressed: (){ },
+        child: Column(
+          children: const [
+SizedBox(height: 4,),
+            Icon(Icons.add,),
+            Text('الصلاة',
+              style: TextStyle(
+                  fontSize: 12
+              ),
+
+            )
+          ],
+        )
+
+    ) ;
   }
 }
