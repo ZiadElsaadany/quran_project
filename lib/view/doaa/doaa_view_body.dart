544dccc1b69f6a0fce765505209fccb57d34cd6d
Ziad Elsdaadany <ziadelsaadany73@gmail.com>
2023-02-06 00:46:52 +0200

    add animation for doaa view

--- lib/view/doaa/doaa_view_body.dart
+++ lib/view/doaa/doaa_view_body.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:quran_project/animation/fade_animation.dart';
 import 'package:quran_project/controller/providers/doaa_provider.dart';
 import 'package:quran_project/view/doaa/toggle_widget.dart';
-import 'package:quran_project/view/widgets/list_ahadeth_and_doaa_widget.dart';
 
 import '../../constant/images_constant.dart';
 import '../../constant/size_constant.dart';
@@ -20,66 +20,69 @@ class DoaaViewBody extends StatelessWidget {
         builder: (context,provider,_) {
           return SizedBox(
             width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(height: returnHeightMediaQuery(ctx: context, size: 0.05),),
-                const ToggleWidget(),
-                SizedBox(height: returnHeightMediaQuery(ctx: context, size: 0.03),),
-              Expanded(
-                  child:
+            child: FadeAnimation(
+              1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: returnHeightMediaQuery(ctx: context, size: 0.05),),
+                  const ToggleWidget(),
+                  SizedBox(height: returnHeightMediaQuery(ctx: context, size: 0.03),),
+                Expanded(
+                    child:
 
-              provider.checkClick==false?
-                  provider.doaaAdded==[]||provider.doaaAdded.isEmpty?
-                  const EmptyDoaa()  :
-                  ListView.builder(
-                    physics: const BouncingScrollPhysics(),
-                    itemBuilder: (ctx,index) {
-                      return index == 9? Column(
-                        children: [
-                          DoaaCard(
-                            title:provider.doaaAdded[index].doaaName??'',
-                            content: provider.doaaAdded[index].doaaContent??'',
-                          ),
-                          Image.asset(ImageConstant.image,
-                            height: returnHeightMediaQuery(ctx: context, size: 0.1),
+                provider.checkClick==false?
+                    provider.doaaAdded==[]||provider.doaaAdded.isEmpty?
+                    const EmptyDoaa()  :
+                    ListView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      itemBuilder: (ctx,index) {
+                        return index == provider.doaaAdded.length-1? Column(
+                          children: [
+                            DoaaCard(
+                              title:provider.doaaAdded[index].doaaName??'',
+                              content: provider.doaaAdded[index].doaaContent??'',
+                            ),
+                            Image.asset(ImageConstant.image,
+                              height: returnHeightMediaQuery(ctx: context, size: 0.1),
 
-                          )
-                        ],
-                      )   :  DoaaCard(
-                        title:provider.doaaAdded[index].doaaName??'',
-                        content: provider.doaaAdded[index].doaaContent??'',
-                      );
-                    },
-                    itemCount: provider.doaaAdded.length,
-                  ) :
+                            )
+                          ],
+                        )   :  DoaaCard(
+                          title:provider.doaaAdded[index].doaaName??'',
+                          content: provider.doaaAdded[index].doaaContent??'',
+                        );
+                      },
+                      itemCount: provider.doaaAdded.length,
+                    ) :
 
-              ListView.builder(
-                physics: const BouncingScrollPhysics(),
-                itemBuilder: (ctx,index) {
-                  return index == 9? Column(
-                    children: [
-                      DoaaCard(
-                        title:provider.doaa[index]['name'],
-                        content: provider.doaa[index]['text'],
-                      ),
-                      Image.asset(ImageConstant.image,
-                        height: returnHeightMediaQuery(ctx: context, size: 0.1),
+                   ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemBuilder: (ctx,index) {
+                    return index == provider.doaa.length-1? Column(
+                      children: [
+                        DoaaCard(
+                          title:provider.doaa[index]['name'],
+                          content: provider.doaa[index]['text'],
+                        ),
+                        Image.asset(ImageConstant.image,
+                          height: returnHeightMediaQuery(ctx: context, size: 0.1),
 
-                      )
-                    ],
-                  )   :  DoaaCard(
-                    title:provider.doaa[index]['name'],
-                    content: provider.doaa[index]['text'],
-                  );
-                },
-                itemCount: provider.doaa.length,
-              )
-
-
-                ,
+                        )
+                      ],
+                    )   :  DoaaCard(
+                      title:provider.doaa[index]['name'],
+                      content: provider.doaa[index]['text'],
+                    );
+                  },
+                  itemCount: provider.doaa.length,
                 )
-              ],
+
+
+                  ,
+                  )
+                ],
+              ),
             ),
           );
         }
